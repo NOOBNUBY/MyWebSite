c7a681732e130265c333284285eea23e90625197
NOOBNUBY <noobnuby77@gmail.com>
2023-06-18 13:55:04 +0900

    :memo: Add png

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='flex justify-center'>
-        <div className='flex items-center justify-center bg-[#6277BF] rounded-md w-[280px] h-[566px] drop-shadow-[19px_19px_10px_rgba(0,0,0,0.25)]'>
-          hello
-        </div>
-    </main>
-  )
+      <main className="flex justify-center items-center">
+          <div className="flex justify-center bg-[#6277BF] rounded-md w-[280px] h-[566px] drop-shadow-[19px_19px_10px_rgba(0,0,0,0.25)] mt-[7%]">
+              <Image
+                  src="/profile.png"
+                  width={10}
+                  height={10}
+                  alt="Picture of the author"
+              />
+          </div>
+      </main>
+  );
 }
