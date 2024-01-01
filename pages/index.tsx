34cff3d8149bb0130a7692992133bcbda06bd853
NOOBNUBY <noobnuby77@gmail.com>
2024-01-01 18:32:55 +0900

    Copyright

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image'
-import Typewriter from 'typewriter-effect';
-import Head from 'next/head'
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
+import Head from "next/head";
 
 function github() {
     window.open("https://github.com/noobnuby");
@@ -12,77 +12,86 @@ function discord() {
     window.open("https://discord.com/users/878092667156856882");
 }
 function easteregg() {
-    alert('Easter Egg!🥚');
+    alert("Easter Egg!🥚");
 }
 
 export default function Home() {
-  return (
-      <main className="flex justify-center items-center font opacity-80 font['Pretendard']">
-          <div className="flex items-center flex-col bg-[#6277BF] rounded-[15px] w-[280px] h-[566px] drop-shadow-[19px_19px_10px_rgba(0,0,0,0.25)] mt-[7%]">
-              <Head>
-                  <title>Noobnuby Profile Site</title>
-                  <link rel="icon" href="/favicon.png" sizes="any"/>
-              </Head>
-              <Image
-                  src="/favicon.png"
-                  width={96}
-                  height={1}
-                  alt=""
-                  onClick={easteregg}
-                  className="mt-[30px]"
-              />
-              <p className="text-[56px]">뉴비</p>
-              <div className="flex mb-[10px]">
-                  <Image
-                      src="/image/discord.png"
-                      width={25}
-                      height={1}
-                      alt=""
-                      onClick={discord}
-                      className="ml-[25px] mr-[25px] hover:cursor-pointer"
-                  />
-                  <Image
-                      src="/image/github.png"
-                      width={25}
-                      height={1}
-                      alt=""
-                      onClick={github}
-                      className="mr-[25px] hover:cursor-pointer"
-                  />
-                  <Image
-                      src="/image/envelope.png"
-                      width={25}
-                      height={1}
-                      alt=""
-                      onClick={email}
-                      className="mr-[25px] hover:cursor-pointer"
-                  />
-              </div>
-              <div>
-                  <Typewriter
-                      onInit={(typewriter) => {
-                          typewriter
-                              .typeString("#Hello, World!")
-                              .pauseFor(3500)
-                              .deleteAll()
-                              .typeString("#Age 15(14)")
-                              .pauseFor(3500)
-                              .deleteAll()
-                              .typeString("#초보 개발자")
-                              .pauseFor(3500)
-                              .deleteAll()
-                              .typeString("#바부")
-                              .pauseFor(3500)
-                              .deleteAll()
-                              .start();
-                      }}
-                  />
-              </div>
-              <div className="mt-[30%] text-[25px]">
-                  <p className="">404</p>
-                  <p className="">Not Found</p>
-              </div>
-          </div>
-      </main>
-  );
+    return (
+        <main className="flex justify-center items-center opacity-80 font['Pretendard']">
+            <div className="flex items-center flex-col bg-[#6277BF] rounded-[15px] w-[280px] h-[566px] drop-shadow-[19px_19px_10px_rgba(0,0,0,0.25)] mt-[7%]">
+                <Head>
+                    <title>Noobnuby Profile Site</title>
+                    <link rel="icon" href="/favicon.png" sizes="any" />
+                </Head>
+                <Image
+                    src="/favicon.png"
+                    width={96}
+                    height={1}
+                    alt=""
+                    onClick={easteregg}
+                    className="mt-[30px]"
+                />
+                <p className="text-[56px]">뉴비</p>
+                <div className="flex mb-[10px]">
+                    <Image
+                        src="/image/discord.png"
+                        width={25}
+                        height={1}
+                        alt=""
+                        onClick={discord}
+                        className="ml-[25px] mr-[25px] hover:cursor-pointer"
+                    />
+                    <Image
+                        src="/image/github.png"
+                        width={25}
+                        height={1}
+                        alt=""
+                        onClick={github}
+                        className="mr-[25px] hover:cursor-pointer"
+                    />
+                    <Image
+                        src="/image/envelope.png"
+                        width={25}
+                        height={1}
+                        alt=""
+                        onClick={email}
+                        className="mr-[25px] hover:cursor-pointer"
+                    />
+                </div>
+                <div>
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString("#Hello, World!")
+                                .pauseFor(3500)
+                                .deleteAll()
+                                .typeString("#Age 15(14)")
+                                .pauseFor(3500)
+                                .deleteAll()
+                                .typeString("#초보 개발자")
+                                .pauseFor(3500)
+                                .deleteAll()
+                                .typeString("#바부")
+                                .pauseFor(3500)
+                                .deleteAll()
+                                .start();
+                        }}
+                    />
+                </div>
+                <div className="mt-[30%] text-[25px]">
+                    <p className="">404</p>
+                    <p className="">Not Found</p>
+                </div>
+                <footer>
+                    <div className="absolute bottom-5 right-5">
+                        <p className="text-[10px] mt-[45%]">
+                            &#169; Copyright 2023{" "}
+                            <a href="https://github.com/noobnuby">noobnuby</a>
+                        </p>
+                        <p className="text-[10px] text-right">All rights reserved</p>
+                    </div>
+                </footer>
+            </div>
+        </main>
+    );
 }
